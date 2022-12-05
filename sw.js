faa6e6daeeccca42c7b3454e373d6cee5a5d3dc8
Susana <susy.alba123@gmail.com>
2022-12-05 13:42:30 -0600

    Feat Service worker was added

--- sw.js
+++ sw.js
@@ -1,30 +1,36 @@
-const STATIC_CACHE = "static-v1";
-
-const APP_SHELL = [
-  "/",
-  "index.html",
-  "style/style.css",
-  "script/main.js",
-  "images/logo_chanel.jpg",
-];
-
-self.addEventListener("install", (e) => {
-  const cacheStatic = caches
-    .open(STATIC_CACHE)
-    .then((cache) => cache.addAll(APP_SHELL));
-
-  e.waitUntil(cacheStatic);
+self.addEventListener('install', e =>{
+  const cacheProm = caches.open('cache-v1')
+      .then(cache => {
+          return cache.addAll([
+              'index.html',
+              './css/index.css',
+              './js/app.js',
+              './json/manifest.json'
+          ])
+          
+      });
+  e.waitUntil(cacheProm);
 });
 
-self.addEventListener("fetch", (e) => {
-  console.log("fetch! ", e.request);
-  e.respondWith(
-    caches
-      .match(e.request)
-      .then((res) => {
-        return res || fetch(e.request);
-      })
-      .catch(console.log)
-  );
-  //   e.waitUntil(response);
+self.addEventListener('fetch', e =>{
+  //cache with network fallback
+  const respuesta = caches.match( e.request )
+      .then ( res => {
+          if ( res ) return res;
+          //no existe el archivo
+          //tengo que ir a la web
+          console.log('No existe', e.request.url);
+          return fetch( e.request ).then ( newResp => {
+              caches.open('cache-v1')
+                  .then( cache => {
+                      cache.put( e.request, newResp);
+                  }
+
+                  )
+              return newResp.clone;
+          });
+      });
+      e.respondWith(respuesta);
+  //only cache
+  //e.respondWith( caches.match(e.request));
 });
